20c4a110639a91da63cde4b2fef8cf99aa27cc1c
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2022-12-19 18:02:13 +0100

    Update with small changes and live emacs theme reload

--- dotfiles/doom.d/custom.el
+++ dotfiles/doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" default))
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" default))
  '(warning-suppress-types
    '((org-element-cache)
      ((yasnippet zombie))
