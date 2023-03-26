fc9cefd462591f6e2f874e97c577948161b7cb26
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-03-26 21:43:40 +0200

    update

--- nix/hosts/spinorbundle/hardware-configuration.nix
+++ nix/hosts/spinorbundle/hardware-configuration.nix
@@ -12,66 +12,50 @@
 # 888   88888888 888  888 "Y8888b. 888  888 888     888    888 888 88888888 "Y8888b.
 # Y88b. Y8b.     888  888      X88 Y88..88P 888     888    888 888 Y8b.          X88
 #  "Y888 "Y8888  888  888  88888P'  "Y88P"  888     888    888 888  "Y8888   88888P'
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  ...
-}: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+{ config, lib, pkgs, modulesPath, ... }: {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
-  boot.blacklistedKernelModules = ["radeon" "amdgpu"];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  boot.blacklistedKernelModules = [ "radeon" "amdgpu" ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/038653da-cdc9-4501-b33e-6bd34872fc70";
+    device = "/dev/disk/by-label/root";
     fsType = "btrfs";
-    options = ["subvol=root" "compress=zstd" "noatime"];
+    options = [ "subvol=root" "compress=zstd" "noatime" ];
   };
 
-  boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/a34dca7f-da86-485d-85a6-26c6fb945d77";
-
-  # Note: not using home anymore
-  # fileSystems."/home" = {
-  #   device = "/dev/disk/by-uuid/038653da-cdc9-4501-b33e-6bd34872fc70";
-  #   fsType = "btrfs";
-  #   options = ["subvol=home" "compress=zstd" "noatime"];
-  # };
+  boot.initrd.luks.devices."enc".device = "/dev/disk/by-label/root_crypt";
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/038653da-cdc9-4501-b33e-6bd34872fc70";
+    device = "/dev/disk/by-label/root";
     fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
+    options = [ "subvol=nix" "compress=zstd" "noatime" ];
   };
 
   fileSystems."/persist" = {
-    device = "/dev/disk/by-uuid/038653da-cdc9-4501-b33e-6bd34872fc70";
+    device = "/dev/disk/by-label/root";
     fsType = "btrfs";
-    options = ["subvol=persist" "compress=zstd" "noatime"];
+    options = [ "subvol=persist" "compress=zstd" "noatime" ];
     neededForBoot = true;
   };
 
   fileSystems."/var/log" = {
-    device = "/dev/disk/by-uuid/038653da-cdc9-4501-b33e-6bd34872fc70";
+    device = "/dev/disk/by-label/root";
     fsType = "btrfs";
-    options = ["subvol=log" "compress=zstd" "noatime"];
+    options = [ "subvol=log" "compress=zstd" "noatime" ];
     neededForBoot = true;
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/349F-D1BD";
+    device = "/dev/disk/by-label/boot";
     fsType = "vfat";
   };
 
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/9625d90d-5d84-48a7-9082-a0ae54287d84";}
-  ];
+  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -83,5 +67,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- nix/profiles/home-dmenu-pywaled.nix
+++ nix/profiles/home-dmenu-pywaled.nix
@@ -12,37 +12,26 @@
 # 888   88888888 888  888 "Y8888b. 888  888 888     888    888 888 88888888 "Y8888b.
 # Y88b. Y8b.     888  888      X88 Y88..88P 888     888    888 888 Y8b.          X88
 #  "Y888 "Y8888  888  888  88888P'  "Y88P"  888     888    888 888  "Y8888   88888P'
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  user,
-  ...
-}: let
+{ config, pkgs, lib, inputs, user, ... }:
+let
   _ = lib.mkOverride 500;
   dmenu-pywaled = let
     name = "dmenu_run";
-    buildInputs = with pkgs; [
-      pywal
-    ];
+    buildInputs = with pkgs; [ pywal ];
     script = pkgs.writeShellScriptBin name ''
       . "''${HOME}/.cache/wal/colors.sh"
 
-      ${pkgs.dmenu-rs}/bin/dmenu_run -w-nb "$color0" --nf "$color15" --sb "$color1" --sf "$color15"
+      ${pkgs.dmenu-rs}/bin/dmenu_run -w -nb "$color0" --nf "$color15" --sb "$color1" --sf "$color15"
     '';
-  in
-    pkgs.symlinkJoin {
-      inherit name;
-      paths = [script pkgs.dmenu-rs] ++ buildInputs;
-      buildInputs = [pkgs.makeWrapper];
-      postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-    };
+  in pkgs.symlinkJoin {
+    inherit name;
+    paths = [ script pkgs.dmenu ] ++ buildInputs;
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+  };
 in {
   home-manager.users.${user} = {
     # home.file.".config/lf/icons".source = _ ./icons;
-    home.packages = with pkgs; [
-      dmenu-pywaled
-    ];
+    home.packages = with pkgs; [ dmenu-pywaled ];
   };
 }
