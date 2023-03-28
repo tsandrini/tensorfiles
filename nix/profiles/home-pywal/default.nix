6532db7e3af560a346e46996edc0d738626f26d5
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-03-28 18:18:03 +0200

    update

--- nix/profiles/home-pywal/default.nix
+++ nix/profiles/home-pywal/default.nix
@@ -18,12 +18,11 @@ let
   cfg = config.home-manager.users.${user};
 in {
   home-manager.users.${user} = {
-    home.packages = with pkgs; [ pywal python2 ];
+    home.packages = with pkgs; [ pywal ];
 
     # Setup general templates
     home.file."${cfg.xdg.configHome}/wal/templates/Xresources".source =
       _ ./templates/Xresources;
-
     systemd.user.tmpfiles.rules = [
       "L ${cfg.home.homeDirectory}/.Xresources - - - - ${cfg.xdg.cacheHome}/wal/Xresources"
     ];
--- nix/profiles/xmonad-with-xmobar-pywaled/default.nix
+++ nix/profiles/xmonad-with-xmobar-pywaled/default.nix
@@ -13,7 +13,61 @@
 # Y88b. Y8b.     888  888      X88 Y88..88P 888     888    888 888 Y8b.          X88
 #  "Y888 "Y8888  888  888  88888P'  "Y88P"  888     888    888 888  "Y8888   88888P'
 { config, pkgs, lib, inputs, user, ... }:
-let _ = lib.mkOverride 500;
+let
+  _ = lib.mkOverride 500;
+  cfg = config.home-manager.users.${user};
+
+  trayerPaddingIcon = pkgs.writeShellScriptBin "trayer-padding-icon.sh" ''
+    #!/bin/sh
+    # Copied from https://github.com/jaor/xmobar/issues/239#issuecomment-233206552
+    # Detects the width of running trayer-srg window (xprop name 'panel')
+    # and creates an XPM icon of that width, 1px height, and transparent.
+    # Outputs an <icon>-tag for use in xmobar to display the generated
+    # XPM icon.
+    #
+    # Run script from xmobar:
+    # `Run Com "/where/ever/trayer-padding-icon.sh" [] "trayerpad" 10`
+    # and use `%trayerpad%` in your template.
+
+
+    # Function to create a transparent Wx1 px XPM icon
+    create_xpm_icon () {
+        timestamp=$(date)
+        pixels=$(for i in `seq $1`; do echo -n "."; done)
+
+        cat << EOF > "$2"
+    /* XPM *
+    static char * trayer_pad_xpm[] = {
+    /* This XPM icon is used for padding in xmobar to */
+    /* leave room for trayer-srg. It is dynamically   */
+    /* updated by by trayer-padding-icon.sh which is run  */
+    /* by xmobar.                                     */
+    /* Created: ''${timestamp} */
+    /* <w/cols>  <h/rows>  <colors>  <chars per pixel> */
+    "$1 1 1 1",
+    /* Colors (none: transparent) */
+    ". c none",
+    /* Pixels */
+    "$pixels"
+    };
+    EOF
+    }
+
+    # Width of the trayer window
+    width=$(xprop -name panel | grep 'program specified minimum size' | cut -d ' ' -f 5)
+
+    # Icon file name
+    iconfile="/tmp/trayer-padding-''${width}px.xpm"
+
+    # If the desired icon does not exist create it
+    if [ ! -f $iconfile ]; then
+        create_xpm_icon $width $iconfile
+    fi
+
+    # Output the icon tag for xmobar
+    echo "<icon=''${iconfile}/>"
+  '';
+
 in {
   services.xserver = {
     enable = _ true;
@@ -49,7 +103,6 @@ in {
       enable = _ true;
       scriptPath = _ ".xinitrc";
 
-      # TODO should investigate more probably
       windowManager.command = lib.mkOverride 50 "exec xmonad";
       windowManager.xmonad = {
         enable = _ true;
@@ -57,5 +110,20 @@ in {
         enableContribAndExtras = _ true;
       };
     };
+
+    # xmobar source
+    home.file."${cfg.xdg.configHome}/wal/templates/xmobarrc".source =
+      _ ./templates/xmobarrc;
+    systemd.user.tmpfiles.rules = [
+      "L ${cfg.xdg.configHome}/xmobar/xmobarrc - - - - ${cfg.xdg.cacheHome}/wal/xmobarrc"
+    ];
+
+    # xmobar trayer padding icon generator
+    home.file."${cfg.xdg.configHome}/xmobar/trayer-padding-icon.sh".source =
+      _ trayerPaddingIcon;
+
+    # lil haskell icon ^^
+    home.file."${cfg.xdg.configHome}/.xmonad/xpm/haskell_20.xpm".source =
+      _ ./xpm/haskell_20.xpm;
   };
 }
