21c214a88c6d6d0f5e7b5113974bb30eeb972bb9
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-04-06 14:57:26 +0200

    update

--- nix/roles/base.nix
+++ nix/roles/base.nix
@@ -39,10 +39,10 @@
     nixosProfiles.system-packages
     nixosProfiles.persist-btrfs
     nixosProfiles.localization
-    nixosProfiles.networking-nm
+    # nixosProfiles.networking-nm
     nixosProfiles.home-manager
-    nixosProfiles.xmonad-with-xmobar-pywaled
-    nixosProfiles.home-xdg
+    # nixosProfiles.xmonad-with-xmobar-pywaled
+    # nixosProfiles.home-xdg
     nixosProfiles.home-zsh
     nixosProfiles.home-neovim
     nixosProfiles.home-pywal
