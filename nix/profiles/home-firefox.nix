6f570b7028dffd289f95c0f3510ce23ff720011d
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-04-09 10:12:42 +0200

    update

--- nix/profiles/home-firefox.nix
+++ nix/profiles/home-firefox.nix
@@ -58,32 +58,8 @@ in {
 
     programs.firefox = {
       enable = _ true;
+      package = pkgs.firefox;
       # package = pkgs.firefox-devedition-bin;
-      package = pkgs.wrapFirefox pkgs.firefox-devedition-bin-unwrapped {
-          extraPolicies = {
-              CaptivePortal = false;
-              DisableFirefoxStudies = true;
-              DisablePocket = true;
-              DisableTelemetry = true;
-              DisableFirefoxAccounts = false;
-              NoDefaultBookmarks = true;
-              OfferToSaveLogins = false;
-              OfferToSaveLoginsDefault = false;
-              PasswordManagerEnabled = false;
-              FirefoxHome = {
-                  Search = true;
-                  Pocket = false;
-                  Snippets = false;
-                  TopSites = false;
-                  Highlights = false;
-              };
-              UserMessaging = {
-                  ExtensionRecommendations = false;
-                  SkipOnboarding = true;
-              };
-          };
-      };
-
       profiles.${user} = {
         id = _ 0;
         name = _ "${user}";
