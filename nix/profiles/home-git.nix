a31119975f4c02557c1b4593df31da5414ec1c47
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-05-02 10:57:54 +0200

    update

--- nix/profiles/home-git.nix
+++ nix/profiles/home-git.nix
@@ -13,9 +13,7 @@
 # Y88b. Y8b.     888  888      X88 Y88..88P 888     888    888 888 Y8b.          X88
 #  "Y888 "Y8888  888  888  88888P'  "Y88P"  888     888    888 888  "Y8888   88888P'
 { config, pkgs, lib, inputs, user, ... }:
-let
-  _ = lib.mkOverride 500;
-  cfg = config.home-manager.users.${user};
+let _ = lib.mkOverride 500;
 in {
 
   home-manager.users.${user} = {
@@ -31,52 +29,50 @@ in {
       userName = _ "${user}";
       userEmail = _ "tomas.sandrini@seznam.cz"; # TODO
       extraConfig = { github.user = _ "${user}"; };
-    };
-
-    # TODO modularize shell
-    programs.zsh.shellAliases = lib.mkIf cfg.programs.zsh.enable {
-      b = _ "git branch";
-      bl = _ "git branch";
-      bd = _ "git branch -d";
-      bdf = _ "git branch -D";
-      f = _ "git fetch";
-      fo = _ "git fetch origin";
-      c = _ "git commit";
-      ca = _ "git commit -a";
-      ch = _ "git checkout";
-      chb = _ "git checkout -b";
-      chr = _ "git checkout --";
-      chra = _ "git checkout -- .";
-      cl = _ "git clone";
-      ph = _ "git push";
-      phu = _ "git push --set-upstream";
-      phuo = _ "git push --set-upstream origin";
-      phuof = _ "git push --set-upstream --force origin";
-      phuf = _ "git push --set-upstream --force";
-      phf = _ "git push --force";
-      pl = _ "git pull";
-      plf = _ "git pull --force";
-      plo = _ "git pull origin";
-      plof = _ "git pull origin --force";
-      a = _ "git add";
-      aa = _ "git add *";
-      r = _ "git reset HEAD";
-      ra = _ "git reset HEAD *";
-      s = _ "git status";
-      sh = _ "git stash";
-      sha = _ "git stash apply";
-      rmc = _ "git rm --cached";
-      m = _ "git merge";
-      mf = _ "git merge --force";
-      i = _ "git init";
-      d = _ "git diff";
-      l = _ "git log";
-      lg1 = _
-        "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
-      lg2 = _
-        "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
-      lg3 = _
-        "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+      aliases = {
+        b = _ "branch";
+        bl = _ "branch";
+        bd = _ "branch -d";
+        bdf = _ "branch -D";
+        f = _ "fetch";
+        fo = _ "fetch origin";
+        c = _ "commit";
+        ca = _ "commit -a";
+        ch = _ "checkout";
+        chb = _ "checkout -b";
+        chr = _ "checkout --";
+        chra = _ "checkout -- .";
+        cl = _ "clone";
+        ph = _ "push";
+        phu = _ "push --set-upstream";
+        phuo = _ "push --set-upstream origin";
+        phuof = _ "push --set-upstream --force origin";
+        phuf = _ "push --set-upstream --force";
+        phf = _ "push --force";
+        pl = _ "pull";
+        plf = _ "pull --force";
+        plo = _ "pull origin";
+        plof = _ "pull origin --force";
+        a = _ "add";
+        aa = _ "add *";
+        r = _ "reset HEAD";
+        ra = _ "reset HEAD *";
+        s = _ "status";
+        sh = _ "stash";
+        sha = _ "stash apply";
+        rmc = _ "rm --cached";
+        m = _ "merge";
+        mf = _ "merge --force";
+        i = _ "init";
+        d = _ "diff";
+        l = _ "log";
+        lg1 = _
+          "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+        lg2 = _
+          "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        lg3 = _
+          "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+      };
     };
   };
 }
