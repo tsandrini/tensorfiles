cc8511b5f97dec2af8314fbdc7110a03a94d709c
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-11-05 11:52:10 +0100

    update spinorbundle

--- hosts/spinorbundle/default.nix
+++ hosts/spinorbundle/default.nix
@@ -60,8 +60,6 @@
   services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
       START_CHARGE_THRESH_BAT1 = 75;
       STOP_CHARGE_THRESH_BAT1 = 80;
     };
