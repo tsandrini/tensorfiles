4b76d28d9a3be497738f96c805b7188057c0316b
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-11-10 18:54:53 +0100

    update

--- hosts/spinorbundle/default.nix
+++ hosts/spinorbundle/default.nix
@@ -64,6 +64,10 @@
   hardware.opengl.driSupport = true;
   # For 32 bit applications
   hardware.opengl.driSupport32Bit = true;
+  # for Southern Islands (SI ie. GCN 1) cards
+  # boot.kernelParams = [ "radeon.si_support=0" "amdgpu.si_support=1" ];
+  # for Sea Islands (CIK ie. GCN 2) cards
+  boot.kernelParams = [ "radeon.cik_support=0" "amdgpu.cik_support=1" ];
 
   # Services
   services.tlp = {
@@ -80,6 +84,7 @@
   hardware.opengl.extraPackages = with pkgs; [
     rocm-opencl-icd
     rocm-opencl-runtime
+    amdvlk
   ];
 
   # hardware.opengl = {
