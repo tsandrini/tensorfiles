26cded1a5999cbe401871d1acf889239c1d7c074
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2023-11-11 14:55:23 +0100

    feat(devenv): update devenv with devcontainers and other features

--- shells/devenv.nix
+++ shells/devenv.nix
@@ -15,31 +15,61 @@ in
     inherit inputs pkgs;
     modules = [
       {
-        packages = with pkgs; [nil shellcheck shfmt markdownlint-cli typos commitizen cz-cli];
+        packages = with pkgs; [
+          cowsay
+          fortune
+          lolcat
+          nil
+          shellcheck
+          shfmt
+          markdownlint-cli
+          typos
+          commitizen
+          cz-cli
+        ];
 
         languages.nix.enable = true;
-        pre-commit.hooks = {
-          # nix
-          alejandra.enable = true;
-          statix.enable = true;
-          deadnix.enable = true;
-          nil.enable = true;
-          # shell
-          shellcheck.enable = true;
-          shfmt.enable = true;
-          # git
-          commitizen.enable = true;
-          # markdown
-          markdownlint.enable = true;
-          # spell checking
-          typos.enable = true;
+        difftastic.enable = true;
+        devcontainer.enable = true; # if anyone needs it
+
+        devenv = {
+          flakesIntegration = true;
         };
 
-        pre-commit.settings = {
-          deadnix.exclude = ["etc"];
-          alejandra.exclude = ["etc"];
-          statix.ignore = ["etc"];
+        pre-commit = {
+          excludes = ["etc"];
+          hooks = {
+            # nix
+            alejandra.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+            nil.enable = true;
+            # shell
+            # shellcheck.enable = true;
+            # shfmt.enable = true;
+            # git
+            commitizen.enable = true;
+            # markdown
+            markdownlint.enable = true;
+            # spell checking
+            typos.enable = true;
+            # github actions
+            actionlint.enable = true;
+          };
+          # settings = {
+          #   deadnix.exclude = ["etc"];
+          #   alejandra.exclude = ["etc"];
+          #   statix.ignore = ["etc"];
+          # };
         };
+
+        enterShell = ''
+          echo ""
+          echo "~~ Welcome to tensorfiles devshell! ~~
+
+          [Fortune of the Day] $(fortune)" | cowsay -W 120 -T "U " | lolcat -F 0.3 -p 10 -t
+          echo ""
+        '';
       }
     ];
   }
