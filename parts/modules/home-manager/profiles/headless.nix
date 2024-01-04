1b1119672860a94988183ecaa1a3d5193c914432
tsandrini <tomas.sandrini@seznam.cz>
2024-01-04 01:54:40 +0100

    fix(hm-headless): test

--- parts/modules/home-manager/profiles/headless.nix
+++ parts/modules/home-manager/profiles/headless.nix
@@ -31,7 +31,7 @@ with lib; let
     if impermanenceCheck
     then config.tensorfiles.hm.system.impermanence
     else {};
-  pathToRelative = x: "~/" + (strings.removePrefix "${config.home.homeDirectory}/" x);
+  pathToRelative = strings.removePrefix "${config.home.homeDirectory}/";
 in {
   options.tensorfiles.hm.profiles.headless = with types;
   with tensorfiles.options; {
