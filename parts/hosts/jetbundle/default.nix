b03bdaf9903a340ebb214b11be74672044986105
tsandrini <tomas.sandrini@seznam.cz>
2024-01-10 14:19:08 +0100

    feat(jetbundle): add jetbundle host

---
+++ parts/hosts/jetbundle/default.nix
@@ -0,0 +1,128 @@
+# --- parts/hosts/jetbundle/default.nix
+#
+# Author:  tsandrini <tomas.sandrini@seznam.cz>
+# URL:     https://github.com/tsandrini/tensorfiles
+# License: MIT
+#
+# 888                                                .d888 d8b 888
+# 888                                               d88P"  Y8P 888
+# 888                                               888        888
+# 888888 .d88b.  88888b.  .d8888b   .d88b.  888d888 888888 888 888  .d88b.  .d8888b
+# 888   d8P  Y8b 888 "88b 88K      d88""88b 888P"   888    888 888 d8P  Y8b 88K
+# 888   88888888 888  888 "Y8888b. 888  888 888     888    888 888 88888888 "Y8888b.
+# Y88b. Y8b.     888  888      X88 Y88..88P 888     888    888 888 Y8b.          X88
+#  "Y888 "Y8888  888  888  88888P'  "Y88P"  888     888    888 888  "Y8888   88888P'
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  # -----------------
+  # | SPECIFICATION |
+  # -----------------
+  # Model: Lenovo B51-80
+
+  # --------------------------
+  # | ROLES & MODULES & etc. |
+  # --------------------------
+  imports = [
+    inputs.disko.nixosModules.disko
+    inputs.nixos-hardware.lenovo-thinkpad-x270
+    ./hardware-configuration.nix
+    ./disko.nix
+  ];
+
+  # ------------------------------
+  # | ADDITIONAL SYSTEM PACKAGES |
+  # ------------------------------
+  # environment.systemPackages = with pkgs; [libva-utils];
+
+  # ----------------------------
+  # | ADDITIONAL USER PACKAGES |
+  # ----------------------------
+  # home-manager.users.${user} = {home.packages = with pkgs; [];};
+
+  # ---------------------
+  # | ADDITIONAL CONFIG |
+  # ---------------------
+  tensorfiles = {
+    profiles.headless.enable = true;
+
+    security.agenix.enable = true;
+    programs.shadow-nix.enable = true;
+    system.users.usersSettings."root" = {
+      agenixPassword.enable = true;
+    };
+    system.users.usersSettings."tsandrini" = {
+      isSudoer = true;
+      isNixTrusted = true;
+      agenixPassword.enable = true;
+    };
+  };
+
+  programs.shadow-client.forceDriver = "iHD";
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # TODO plasma stuff
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
+  programs.kdeconnect.enable = true;
+
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+
+  programs.steam.enable = true; # just trying it out
+  programs.partition-manager.enable = true;
+
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+  };
+
+  home-manager.users."tsandrini" = {
+    tensorfiles.hm = {
+      profiles.graphical-plasma.enable = true;
+      security.agenix.enable = true;
+
+      programs.pywal.enable = true;
+      programs.spicetify.enable = true;
+      services.pywalfox-native.enable = true;
+      services.keepassxc.enable = true;
+    };
+
+    services.syncthing = {
+      enable = true;
+      tray.enable = true;
+    };
+
+    home.username = "tsandrini";
+    home.homeDirectory = "/home/tsandrini";
+    home.sessionVariables = {
+      DEFAULT_USERNAME = "tsandrini";
+      DEFAULT_MAIL = "tomas.sandrini@seznam.cz";
+    };
+
+    home.packages = with pkgs; [
+      thunderbird
+      beeper
+      armcord
+      anki
+      shfmt
+      libreoffice
+      neofetch
+      pavucontrol
+      texlive.combined.scheme-medium
+      zotero
+      lapack
+    ];
+  };
+}
