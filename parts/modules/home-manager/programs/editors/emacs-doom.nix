5d5d3be1fb43c25e22c61ad6bdab8383ad4e90a5
tsandrini <tomas.sandrini@seznam.cz>
2024-08-02 11:07:51 +0200

    refactor(emacs-doom): disable emacs service

--- parts/modules/home-manager/programs/editors/emacs-doom.nix
+++ parts/modules/home-manager/programs/editors/emacs-doom.nix
@@ -220,7 +220,7 @@ in
       ];
 
       services.emacs = {
-        enable = _ true;
+        enable = _ false;
         package = _ emacsPkg;
         startWithUserSession = _ "graphical";
       };
