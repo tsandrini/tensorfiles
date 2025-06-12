f8c8ca4021e81d86ae7b1e9949b3616c1dde5055
tsandrini <tomas.sandrini@seznam.cz>
2025-06-12 14:55:44 +0200

    feat(modules/nixos/ssh): harden openssh settings

--- flake-parts/modules/nixos/services/networking/ssh.nix
+++ flake-parts/modules/nixos/services/networking/ssh.nix
@@ -84,10 +84,17 @@ in
           =====================================================================
         '';
         settings = {
-          PermitRootLogin = _ "no";
-          PasswordAuthentication = _ false;
-          StrictModes = _ true;
+          # Additional security hardenings
+          AllowAgentForwarding = _ false;
+          AllowTcpForwarding = _ false;
+          ClientAliveCountMax = _ 2;
           KbdInteractiveAuthentication = _ false;
+          MaxAuthTries = _ 3;
+          MaxSessions = _ 2;
+          PasswordAuthentication = _ false;
+          PermitRootLogin = _ "no";
+          StrictModes = _ true;
+          # TCPKeepAlive = _ false; # TODO: not sure
         };
       };
     }
